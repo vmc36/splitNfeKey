7be886cf3d45f5c35f86ee27ff261c1cfd951507
Vinicius <vmcmodesto@gmail.com>
2022-08-29 19:39:05 -0300

    wip

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-entrada.addEventListener("input", function (e) {
-    const chaveNfe = document.getElementById("entrada").value;
-    const chaveNfearray = chaveNfe.slice("");
-    const splitKeyobj = {
-      uf: chaveNfearray.slice(0, 2),
-      date : chaveNfearray.slice(2, 6),
-      cnpj : chaveNfearray.slice(6, 20),
-      mod : chaveNfearray.slice(20, 22),
-      serie : chaveNfearray.slice(22, 25),
-      numberNfe : chaveNfearray.slice(25, 34),
-      codeNfe : chaveNfearray.slice(34, 43),
-      digito : chaveNfearray.slice(43,44),
-    }
-    alert (JSON.stringify(splitKeyobj));
-   });
-   
+addEventListener("input", function () {
+  const chaveNfe = document.getElementById("entrada").value;
+  var tbody = document.querySelector('tbody');
+  const slicedKey = chaveNfe.slice("");
+
+  if (slicedKey.length === 44) {
+    const splitKeyobj = [{
+      "uf": "uf.: " + slicedKey.slice(0, 2),
+      "date": "Data: " + slicedKey.slice(2, 6),
+      "cnpj": "CNPJ: " + slicedKey.slice(6, 20),
+      "mod": "Modelo da nota: " + slicedKey.slice(20, 22),
+      "serie": "Série da Emissão: " + slicedKey.slice(22, 25),
+      "numberNfe": "Número da Nfe: " + slicedKey.slice(25, 34),
+      "codeNfe": "Código da Nfe no sistema emissor: " + slicedKey.slice(34, 43),
+      "digito": "Digito Verificador: " + slicedKey.slice(43, 44),
+    }];
+    splitKeyobj.forEach(function (relatorio) {
+      var tr = document.createElement('tr');
+      for (var campo in relatorio) {
+        var td = document.createElement('td');
+        td.innerHTML = relatorio[campo];
+        tr.appendChild(td);
+      };
+      tbody.appendChild(tr);
+      document.querySelector('tr').display = "grid";
+    });
+  }
+});
